20cc2a9aee289c1b8602f8ac44b617dace9bfeee
Stephen Crawley <s.crawley@uq.edu.au>
2014-03-18 16:04:02 +1000

    Fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,6 @@ default[:duplicity][:configs] = '/etc/duplicity'
 
 default[:duplicity][:cloudfiles][:bucket] = "backup_#{node[:fqdn]}"
 default[:duplicity][:s3][:bucket] = "backup_#{node[:fqdn]}"
+default[:duplicity][:swift][:container] = "backup_#{node[:fqdn]}"
  
 default[:duplicity][:full_backups_to_keep] = 3
